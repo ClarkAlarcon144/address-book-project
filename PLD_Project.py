fc4e8b36226f6894566bc8c86874cb075320b3aa
Clark Alarcon <clarkalarcon144@gmail.com>
2025-12-23 12:40:22 +0800

    Used list of dictionaries to store contacts instead of using parallel lists. Merged welcome message function into display menu function. Put sorting function when using view contacts function. Added check for empty address book. Added pretty table into search function.

--- PLD_Project.py
+++ PLD_Project.py
@@ -4,38 +4,43 @@ Each function is labeled and commented.
 Please intindihin niyo yung mga ginawa para gets niyo rin pag defense na.
 ----------------------------'''
 
-import json #Ito yung ginagamit para ma-save yung laman ng address book kahit i-close program.
+import json
 import os
-import colorama  #for making the ui colorful
-from colorama import Fore, Back, Style
+import colorama 
+from colorama import Fore, Style
 import time
 from prettytable import PrettyTable
+import sys
 
 colorama.init(autoreset=True, convert=True)
 
-def welcome_message():
-    
-    message = "Hello User, Welcome to our Address Book!"
-    
-    for char in message:
-        
-        print(Style.BRIGHT + Fore.LIGHTCYAN_EX + char, end='', flush=True)
-        time.sleep(0.011)
-    print() 
 
-def input_or_cancel(input_text, cancel_text):
-    value = input(input_text).strip()
+def input_or_cancel(input_prompt, cancel_message):
+    value = input(input_prompt).strip()
 
     if value.lower() == "q":
-        print(cancel_text)
+        print(cancel_message)
         return None
     
     return value
 
-# Function to display the main menu of the address book
-def display_menu():
+def create_contact(first_name, last_name, address, contact_number):
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "address": address,
+        "contact_number": contact_number
+    }
 
-    welcome_message()
+def display_menu():
+    welcome_message = "Hello User, Welcome to our Address Book!"
+    
+    for char in welcome_message:
+        
+        print(Style.BRIGHT + Fore.LIGHTCYAN_EX + char, end='', flush=True)
+        time.sleep(0.011)
+
+    print() 
 
     menu_options = [
         (Fore.LIGHTBLUE_EX, "What would you like to do?"),
@@ -52,27 +57,44 @@ def display_menu():
         time.sleep(0.08)
 
 def save_address_book():
-    # Update the data dictionary with the latest lists.
-    # Kasi iniba-iba natin laman nung lists diba, 
-    # kaya kailangan natin i-update yung data na ibabalik natin sa file ulit
-    data = {
-        "first names": first_name_list,
-        "last names": last_name_list,
-        "addresses": address_list,
-        "contacts": contact_number_list
-    }
+    data = address_book
 
-    # Save all changes back to the JSON file.
-    # I-sasave niya yung laman nung address book dun sa file,
-    # para next time, ma-loload nalang ulit laman nung address book
     with open("address_book.json", "w") as file:
         json.dump(data, file, indent=4)
 
-# Function to add a new contact to the lists
-def add_contact():
-    print("Press (q) at any time to cancel operation.\n")
+def view_contacts():
+    if not address_book:
+        print("\nAddress book is empty.")
+        return
 
-    # Ask user for contact details
+    print("\nList of Contacts:")
+
+    table = PrettyTable()
+
+    table.field_names = [
+        f"{Fore.CYAN}No.{Style.RESET_ALL}",
+        f"{Fore.LIGHTBLUE_EX}First Name{Style.RESET_ALL}",
+        f"{Fore.LIGHTGREEN_EX}Last Name{Style.RESET_ALL}",
+        f"{Fore.LIGHTYELLOW_EX}Address{Style.RESET_ALL}",
+        f"{Fore.LIGHTRED_EX}Contact Number{Style.RESET_ALL}"
+        ]
+    
+    for i, contact in enumerate(address_book):
+        table.add_row(
+            [
+            Fore.CYAN + str(i + 1) + Style.RESET_ALL,
+            Fore.LIGHTBLUE_EX + contact["first_name"] + Style.RESET_ALL,
+            Fore.LIGHTGREEN_EX + contact["last_name"] + Style.RESET_ALL,
+            Fore.YELLOW + contact["address"] + Style.RESET_ALL,
+            Fore.LIGHTRED_EX + contact["contact_number"] + Style.RESET_ALL
+            ]
+        )
+
+    print(table)
+
+def add_contact():
+    print("Press (q) at any time to cancel operation.")
+
     first_name = input_or_cancel("First Name: ", "Add contact cancelled.")
     if first_name is None:
         return
@@ -89,21 +111,131 @@ def add_contact():
     if contact_number is None:
         return
     
-    # Add each piece of information to its respective list
-    first_name_list.append(first_name)
-    last_name_list.append(last_name)
-    address_list.append(address)
-    contact_number_list.append(contact_number)
+    first_name = first_name.title()
+    last_name = last_name.title()
+
+    contact = create_contact(first_name, last_name, address, contact_number)
     
     print("\nContact has been added.")
+    
+    address_book.append(contact)
 
     save_address_book()
 
-def view_contacts():
-    print("List of Contacts:")
+def edit_contact():
+    if not address_book:
+        print("\nAddress book is empty.")
+        return
+    
+    view_contacts()
 
-    # Loop through each contact and display their information
+    print("\nEnter (q) at any time to cancel operation.")
+
+    while True:
+        try:
+            index = input_or_cancel("What number do you want to edit? ", "Edit contact cancelled.")
+            if index is None:
+                return
+            
+            index = int(index)
+
+            # Check if the index is valid
+            if index < 1 or index > len(address_book):
+                print("Invalid entry number.")
+                continue
+        
+        except ValueError:
+            print("\nInvalid input. Please enter a number.\n")
+            continue
+
+        break
+    
+    old_contact = address_book[index-1]
+
+    print("\nInput blank space to keep information as is.\n")
+    
+    first_name = input_or_cancel(f"First Name ({old_contact["first_name"]}): ", "Edit contact cancelled.")
+    if first_name is None:
+        return
+    
+    if first_name == "":
+        first_name = old_contact["first_name"]
+    
+    last_name = input_or_cancel(f"Last Name ({old_contact["last_name"]}): ", "Edit contact cancelled.")
+    if last_name is None:
+        return
+    
+    if last_name == "":
+        last_name = old_contact["last_name"]
+
+    address = input_or_cancel(f"Address ({old_contact["address"]}): ", "Edit contact cancelled.")
+    if address is None:
+        return
+    
+    if address == "":
+        address = old_contact["address"]
+
+    contact_number = input_or_cancel(f"Contact Number ({old_contact["contact_number"]}): ", "Edit contact cancelled.")
+    if contact_number is None:
+        return
+    
+    if contact_number == "":
+        contact_number = old_contact["contact_number"]
+
+    first_name = first_name.title()
+    last_name = last_name.title()
+
+    new_contact = create_contact(first_name, last_name, address, contact_number)
+
+    address_book[index-1] = new_contact
+
+    save_address_book()
+
+    print("\nContact has been edited.")
+
+def delete_contact():
+    if not address_book:
+        print("\nAddress book is empty.")
+        return
+    
+    view_contacts()
+
+    print("\nPress (q) at any time to cancel operation.")
+    
+    while True:
+        try:
+            index = input_or_cancel("What number do you want to delete? ", "Delete contact cancelled.")
+            if index is None:
+                return
+            
+            index = int(index)
+            
+            if index < 1 or index > len(address_book):
+                print("Invalid entry number.")
+                continue
+        
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        break
+
+    confirm = input("\nAre you sure you want to delete this contact? (y/n): ").lower()
+    if confirm != "y":
+        print("\nDelete cancelled.")
+        return
+
+    address_book.pop(index-1)
+
+    save_address_book()
+
+    print("\nContact has been deleted.")
+
+def search(search_term, category):
+    found = False 
+    
     table = PrettyTable()
+    
     table.field_names = [
         f"{Fore.CYAN}No.{Style.RESET_ALL}",
         f"{Fore.LIGHTBLUE_EX}First Name{Style.RESET_ALL}",
@@ -111,148 +243,56 @@ def view_contacts():
         f"{Fore.LIGHTYELLOW_EX}Address{Style.RESET_ALL}",
         f"{Fore.LIGHTRED_EX}Contact Number{Style.RESET_ALL}"
         ]
-
-    for i in range(len(first_name_list)):
-        table.add_row(
-            [
-            Fore.CYAN + str(i + 1) + Style.RESET_ALL,
-            Fore.LIGHTBLUE_EX + first_name_list[i] + Style.RESET_ALL,
-            Fore.LIGHTGREEN_EX + last_name_list[i] + Style.RESET_ALL,
-            Fore.YELLOW + address_list[i] + Style.RESET_ALL,
-            Fore.LIGHTRED_EX + contact_number_list[i] + Style.RESET_ALL
-            ]
-        )
-
-    print(table)
-
-# Function to edit an existing contact
-def edit_contact():
-    view_contacts()
-
-    print("Press (q) at any time to cancel operation.\n")
-
-    while True:
-        try:
-            index = input_or_cancel("What number do you want to edit? ", "Edit contact cancelled.")
-            if index is None:
-                return
-            
-            index = int(index)
-
-            # Check if the index is valid
-            if index < 1 or index > len(first_name_list):
-                print("Invalid entry number.")
-                continue
-        
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        break
-
-    # Update the contact information at the selected index
-    first_name = input_or_cancel("First Name: ", "Edit contact cancelled.")
-    if first_name is None:
-        return
     
-    last_name = input_or_cancel("Last Name: ", "Edit contact cancelled.")
-    if last_name is None:
-        return
+    table.border = True
+    table.header = True
+    table.hrules = True
 
-    address = input_or_cancel("Address: ", "Edit contact cancelled.")
-    if address is None:
-        return
+    for i, contact in enumerate(address_book):
 
-    contact_number = input_or_cancel("Contact Number: ", "Edit contact cancelled.")
-    if contact_number is None:
-        return
-    
-    #Pag tapos na mag-input ng lahat, at hindi nag-cancel yung user, then i-eedit na yung lists.
-    first_name_list[index-1] = first_name
-    last_name_list[index-1] = last_name
-    address_list[index-1] = address
-    contact_number_list[index-1] = contact_number
+        if search_term.lower() in contact[category].lower(): 
+            first_name = contact["first_name"]
+            last_name = contact["last_name"]
+            address = contact["address"]
+            contact_number = contact["contact_number"]
 
-    print("\nContact has been edited.")
+            table.add_row(
+                [
+                Fore.CYAN + str(i + 1) + Style.RESET_ALL,
+                Fore.LIGHTBLUE_EX + first_name + Style.RESET_ALL,
+                Fore.LIGHTGREEN_EX + last_name + Style.RESET_ALL,
+                Fore.YELLOW + address + Style.RESET_ALL,
+                Fore.LIGHTRED_EX + contact_number + Style.RESET_ALL
+                ]
+            )
 
-    save_address_book()
-    
-# Function to delete a contact from the lists
-def delete_contact():
-    view_contacts()
+            found = True 
 
-    print("Press (q) at any time to cancel operation.\n")
-    
-    while True:
-        try:
-            index = input_or_cancel("What number do you want to edit? ", "Edit contact cancelled.")
-            if index is None:
-                return
-            
-            index = int(index)
-            
-            # Check if the index is valid
-            if index < 1 or index > len(first_name_list):
-                print("Invalid entry number.")
-                continue
-        
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
+    if found:
+        print(table)
 
-        break
-    
-    # Remove the contact from all lists
-    first_name_list.pop(index-1)
-    last_name_list.pop(index-1)
-    address_list.pop(index-1)
-    contact_number_list.pop(index-1)
-
-    print("Contact has been deleted.")
-
-    save_address_book()
-
-# Function to search for a contact in any list
-def search(search_term, search_term_list):
-    found = False # Flag to check if any match is found
-
-    # Loop through the search list and check for matches
-    for i, data in enumerate(search_term_list):
-
-        if search_term.lower() == data.lower(): # Titingnan kung may match sa list yung hinahanap mo
-            # Retrieve all contact details
-            first_name = first_name_list[i]
-            last_name = last_name_list[i]
-            address = address_list[i]
-            contact_number = contact_number_list[i]
-    
-            # Print the found contact
-            print(f"{i+1}) {first_name} | {last_name} | {address} | {contact_number}")
-
-            found = True #Update flag
-
-    # If no contact matched, show a message
     if not found:
-        print("Entry does not exist.")
+        print("\nEntry does not exist.")
 
-# Function to search the address book by category
 def search_address_book():
-    print("Press (q) at any time to cancel operation.\n")
+    if not address_book:
+        print("\nAddress book is empty.")
+        return
+    
+    print("\nPress (q) at any time to cancel operation.")
 
     while True:
         print(
-"""
-How do you want to search the address book?
-    (a) by first name
-    (b) by last name
-    (c) by address
-    (d) by contact number
+f"""
+{Fore.LIGHTCYAN_EX}How do you want to search the address book?
+    {Fore.LIGHTRED_EX}(a) by first name
+    {Fore.LIGHTGREEN_EX}(b) by last name
+    {Fore.YELLOW}(c) by address
+    {Fore.LIGHTMAGENTA_EX}(d) by contact number
 
 """
 )
         try:
-
-        # Ask the user what field to search
             answer = input_or_cancel("Answer: ", "Search cancelled.")
             if answer is None:
                 return
@@ -268,78 +308,108 @@ How do you want to search the address book?
             continue
 
         break
-        
-    # Call the search function with the appropriate list
+
     if answer == "a":
         search_term = input_or_cancel("Enter the first name: ", "Search cancelled.")
         if search_term is None:
             return
         
-        search(search_term, first_name_list)
+        search(search_term, "first_name")
 
     elif answer == "b":
-        search_term = input("Enter the last name: ").strip()
+        search_term = input_or_cancel("Enter the last name: ", "Search cancelled.")
+        if search_term is None:
+            return
         
-        search(search_term, last_name_list)
+        search(search_term, "last_name")
         
     elif answer == "c":
-        search_term = input("Enter the address to search: ").strip()
+        search_term = input_or_cancel("Enter the address to search: ", "Search cancelled.")
+        if search_term is None:
+            return
 
-        search(search_term, address_list)
+        search(search_term, "address")
         
     elif answer == "d":
-        search_term = input("Enter the contact number to search: ").strip()
+        search_term = input_or_cancel("Enter the contact number to search: ", "Search cancelled.")
+        if search_term is None:
+            return
         
-        search(search_term, contact_number_list)
-        
-# Function to exit the program
+        search(search_term, "contact_number")
+ 
 def exit_program():
-    quit()
+    sys.exit()
+
+def sort_contacts():
+    print(f"""
+Sort contacts by:
+    (a) First Name (A-Z)
+    (b) First Name (Z-A)
+    (c) Last Name (A-Z)
+    (d) Last Name (Z-A)
+""")
+    
+    while True:
+        choice = input_or_cancel("Choice: ", "Sort contact cancelled.")
+
+        if choice is None:
+            return
+        
+        choice = choice.lower()
+
+        if choice == "a":
+            address_book.sort(key=lambda contact: contact["first_name"].lower())
+            break
+
+        elif choice == "b":
+            address_book.sort(key=lambda contact: contact["first_name"].lower(), reverse=True)
+            break
+        
+        elif choice == "c":
+            address_book.sort(key=lambda contact: contact["last_name"].lower())
+            break
+        
+        elif choice == "d":
+            address_book.sort(key=lambda contact: contact["last_name"].lower(), reverse=True)
+            break
+
+        else:
+            print("\nInvalid input. Only input letters a, b, c, and d.")
+            continue
+    
+    print("\nContacts sorted.")
+
 
 
 # ----------- Main Program Starts Here -----------
 
-# Try to load existing address book, if it exists.
-# Ito yung ginagamit para ma-load yung laman nung address book kahit pinatay mo program
 try:
     with open("address_book.json", "r") as file:
         data = json.load(file)
 
-# If file does not exist, create empty data structure
 except FileNotFoundError:
-    data = {
-        "first names": [],
-        "last names": [],
-        "addresses": [],
-        "contacts": []
-        }
+    data = []
 
-# Separate the JSON data into individual lists for easier access
-first_name_list = data["first names"]
-last_name_list = data["last names"]
-address_list = data["addresses"]
-contact_number_list = data["contacts"]
+address_book = data
 
 option = 0
 
 while option != 6:
     os.system('cls' if os.name == 'nt' else 'clear')
-    
-    # Pakita menu to the user
+
     display_menu()
     
     try:
         option = int(input(f"\nChoose an option: "))
 
         if option < 1 or option > 6:
-            print("Invalid input. Only numbers 1-6 are allowed.")
+            print("\nInvalid input. Only numbers 1-6 are allowed.")
             continue
 
     except ValueError:
-        print("Invalid input. Please enter a number.")  
+        print("\nInvalid input. Please enter a number.")  
         continue
 
-    # Call the appropriate function based on user input
     if option == 1:
         add_contact()
 
@@ -351,6 +421,12 @@ while option != 6:
 
     elif option == 4:
         view_contacts()
+        choice = input("\nSort the contacts? (y/n) ").lower()
+
+        while choice == "y":
+            sort_contacts()
+            view_contacts()
+            choice = input("\nSort the contacts? (y/n) ").lower()
 
     elif option == 5:
         search_address_book()
@@ -358,5 +434,5 @@ while option != 6:
     elif option == 6:
         exit_program()
     
-    input()
+    input("\nEnter any key to show menu again. ")
 
